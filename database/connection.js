c7553208ae007cb8253231b2ababf9c5e233c645
YB122 <youssefbenyamine2eme@gmail.com>
2026-04-01 16:31:09 +0200

    update the code 2

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,13 @@
-
+import dns from "node:dns";
 import mongoose from "mongoose";
+// import { env } from "../../config/env.service.js";
+
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const dataBaseConnection = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27017/nti-test", {
-      serverSelectionTimeoutMS: 5000, // 5 second timeout
+      serverSelectionTimeoutMS: 5000,
       connectTimeoutMS: 5000,
     });
     console.log("data base connected");
